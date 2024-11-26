7c834c474a5f79ceca6156f3b9815d12b2fb90af
Ivan <ivandoska2006@gmail.com>
2024-11-27 00:31:15 +0200

    Design and functionality improved

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''), // English
+        const Locale('ru', ''), // Russian
+        const Locale('lv', ''), // Latvian
+      ],
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
       home: ICDScreen(),
     );
   }
@@ -24,6 +37,8 @@ class ICDScreen extends StatefulWidget {
 class _ICDScreenState extends State<ICDScreen> {
   Map<String, String> _icdData = {};
   String _selectedLanguage = 'ru';
+  String _searchQuery = '';
+  TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
@@ -33,61 +48,101 @@ class _ICDScreenState extends State<ICDScreen> {
 
   Future<void> _loadICDData() async {
     try {
-      // Загрузка данных из файла JSON
       final String data =
           await rootBundle.loadString('assets/icd_$_selectedLanguage.json');
-      setState(() {
-        _icdData = Map<String, String>.from(json.decode(data));
-      });
+      final parsedData = await compute(_parseJson, data);
+      setState(() => _icdData = parsedData);
     } catch (e) {
-      // Логирование ошибок, если файл не найден или содержит ошибку
-      print('Ошибка загрузки JSON: $e');
-      setState(() {
-        _icdData = {
-          "Ошибка": "Не удалось загрузить данные для выбранного языка"
-        };
-      });
+      _showError('Ошибка загрузки данных: $e');
+      setState(() => _icdData = {});
     }
   }
 
+  static Map<String, String> _parseJson(String jsonString) =>
+      Map<String, String>.from(json.decode(jsonString));
+
   void _changeLanguage(String languageCode) {
-    setState(() {
-      _selectedLanguage = languageCode;
-    });
+    setState(() => _selectedLanguage = languageCode);
     _loadICDData();
   }
 
+  void _showError(String message) => ScaffoldMessenger.of(context)
+      .showSnackBar(SnackBar(content: Text(message)));
+
   @override
   Widget build(BuildContext context) {
+    final filteredICDData = _icdData.entries
+        .where((entry) =>
+            entry.key.contains(_searchQuery) ||
+            entry.value.toLowerCase().contains(_searchQuery.toLowerCase()))
+        .toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('МКБ - ${_selectedLanguage.toUpperCase()}'),
-        actions: [
-          DropdownButton<String>(
-            value: _selectedLanguage,
-            onChanged: (value) {
-              if (value != null) _changeLanguage(value);
-            },
-            items: [
-              DropdownMenuItem(value: 'ru', child: Text('Русский')),
-              DropdownMenuItem(value: 'en', child: Text('English')),
-              DropdownMenuItem(value: 'lv', child: Text('Latviešu')),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(65.0), // Устанавливаем высоту AppBar
+        child: AppBar(
+          backgroundColor: Colors.blue, // Синий цвет панели
+          centerTitle: true,
+          title: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _searchController,
+                  onChanged: (query) => setState(() => _searchQuery = query),
+                  decoration: InputDecoration(
+                    hintText: 'Поиск...',
+                    hintStyle: TextStyle(color: Colors.white70),
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.8),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  style: TextStyle(color: Colors.black),
+                  cursorColor: Colors.blue,
+                ),
+              ),
+              SizedBox(width: 10), // Отступ между строкой поиска и меню
+              DropdownButtonHideUnderline(
+                child: DropdownButton<String>(
+                  value: _selectedLanguage,
+                  onChanged: (value) {
+                    if (value != null) _changeLanguage(value);
+                  },
+                  icon: SizedBox.shrink(), // Убираем иконку раскрытия
+                  dropdownColor: Colors.white, // Цвет выпадающего меню
+                  items: [
+                    DropdownMenuItem(value: 'ru', child: Text('Русский')),
+                    DropdownMenuItem(value: 'en', child: Text('English')),
+                    DropdownMenuItem(value: 'lv', child: Text('Latviešu')),
+                  ],
+                ),
+              ),
             ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: filteredICDData.isEmpty
+                ? Center(
+                    child: _icdData.isEmpty
+                        ? CircularProgressIndicator()
+                        : Text('Ничего не найдено'),
+                  )
+                : ListView.builder(
+                    itemCount: filteredICDData.length,
+                    itemBuilder: (context, index) {
+                      final code = filteredICDData[index].key;
+                      final name = filteredICDData[index].value;
+                      return ListTile(title: Text('$code: $name'));
+                    },
+                  ),
           ),
         ],
       ),
-      body: _icdData.isEmpty
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: _icdData.length,
-              itemBuilder: (context, index) {
-                final code = _icdData.keys.elementAt(index);
-                final name = _icdData[code];
-                return ListTile(
-                  title: Text('$code: $name'),
-                );
-              },
-            ),
     );
   }
 }
